4c3a3d60b83cec3df12e6b8796729ddfe1ffbfc9
swonb <47047914+swonb@users.noreply.github.com>
2020-07-06 13:49:17 -0400

    Added Navigation (#19~21)

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import { GlobalStyles } from "../styles/Global";
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails");
+  };
+
   return (
     <View style={GlobalStyles.container}>
       <Text style={GlobalStyles.titleText}>Home Screen</Text>
+      <Button title="go to review dets" onPress={pressHandler} />
     </View>
   );
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import { GlobalStyles } from "../styles/Global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={GlobalStyles.container}>
       <Text>ReviewDetails Screen</Text>
+      <Button title="back to home screen" onPress={pressHandler} />
     </View>
   );
 }
